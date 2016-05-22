d8788a32234f2dfc24920c21d8c9f1bf1951705b
iamsachin <sachin.rajput.k@gmail.com>
2016-05-22 20:20:08 +0530

    Changed background

--- public/js/urladder.js
+++ public/js/urladder.js
@@ -15,7 +15,7 @@ $(document).ready(function() {
         list: textlist ,
         startIndex:0,
         trickerPanel: $('#sentences'),
-        interval: 6 * 1000,
+        interval: 4 * 1000,
     });
 
 });
