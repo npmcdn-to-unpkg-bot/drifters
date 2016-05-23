a4a5c1f18ac1277cd348ef2d6a8058c90534d61b
iamsachin <sachin.rajput.k@gmail.com>
2016-05-23 17:09:26 +0530

    Added toasts, layout changes, beautified ugly menu button

--- public/js/submitter.js
+++ public/js/submitter.js
@@ -6,40 +6,53 @@ $('#form-outer').submit(function() {
 
     }else{
     	$.ajax({
-                url: this.action,
-                type: this.method,
-                data: $(this).serialize(),
-                success: function (result) {
-                    if(result == 'success'){
-                    	$('#inputName').attr('disabled', true);
-                    	$('#inputEmail').attr('disabled', true);
-                    	$('#inputPhone').attr('disabled', true);
-                    	$('#inputWhy').attr('disabled', true);
-                    	$('#blog-url').attr('disabled', true);
-                    	$('#rc-imageselect').hide();
-                    	$('#add-url').hide();
-                    	$('#submit').hide().replaceWith("<div class='text-muted'>Welcome aboard. <br>We will mail you soon if you are selected.</div>");
-                    	$('#submit').fadeIn();
-                    }
-                }
-        });
+            url: this.action,
+            type: this.method,
+            data: $(this).serialize(),
+            success: function (result) {
+                if(result == 'success'){
+                 $('#inputName').attr('disabled', true);
+                 $('#inputEmail').attr('disabled', true);
+                 $('#inputPhone').attr('disabled', true);
+                 $('#inputWhy').attr('disabled', true);
+                 $('#blog-url').attr('disabled', true);
+                 $('#rc-imageselect').hide();
+                 $('#add-url').hide();
+                 $('#submit').hide();
+
+                 $('.well').animate({
+                    'marginTop' : "+=60px" 
+                });
+
+                 $.toast({
+                    text : "Welcome aboard. We will mail you soon if you are selected.",
+                    showHideTransition: 'slide',
+                    loader: false,
+                    position: 'bottom-center',
+                    hideAfter: 10000,
+                    allowToastClose: false
+                });
+
+             }
+         }
+     });
     }
     return false;
 });
 $('#inputEmail').blur(function(){
-        $.ajax({
-            url: '/checkmail',
-            type: 'POST',
-            data: {'email' : $('#inputEmail').val()},
-            success: function(result){
-                if(result == 'exists'){
-                    $('#email-error').fadeIn();
-                    allow = false;
-                }else{
-                    $('#email-error').fadeOut();
-                    allow = true;
-                }
+    $.ajax({
+        url: '/checkmail',
+        type: 'POST',
+        data: {'email' : $('#inputEmail').val()},
+        success: function(result){
+            if(result == 'exists'){
+                $('#email-error').fadeIn();
+                allow = false;
+            }else{
+                $('#email-error').fadeOut();
+                allow = true;
             }
+        }
 
-        });
     });
+});
